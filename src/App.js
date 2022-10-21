eb8a803451180b9be2c88e287d7b0f80af9ae324
Melbine <melbineyt@gmail.com>
2022-10-21 16:46:11 +0300

    add custom Hook

--- src/App.js
+++ src/App.js
@@ -1,21 +1,21 @@
-
 import './App.css';
 import {useEffect} from "react";
-const tg = window.Telegram.WebApp
+import {useTelegram} from "./hooks/useTelegram";
+
 function App() {
 
+    const {onToggleButton, tg} = useTelegram();
+
     useEffect(() =>{
-            tg.ready()
+            tg.ready();
     },[])
-    const onClose = () =>{
-        tg.close()
-    }
+
 
 
     return(
         <div className="App">
                 <h1>Work</h1>
-            <button onClick={onClose}>Close</button>
+            <button onClick={onToggleButton}>Close</button>
         </div>
     )
 }
